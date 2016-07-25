a516b08f74a994b8fb5d0fa23a7936c857dc589f
kaleo211 <hexuebin0201@gmail.com>
2016-07-25 16:48:01 -0400

    second

--- main.go
+++ main.go
@@ -2,16 +2,42 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
   "os"
   "net/http"
 )
+
+const FILEPATH = "/mnt/data/goapp.txt"
 
 func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintln(w, "hello world")
 }
 
+
+func read(w http.ResponseWriter, r *http.Request) {
+  content, err := ioutil.ReadFile(FILEPATH)
+  if err != nil {
+    fmt.Fprintln(w, err)
+    return
+  }
+  fmt.Fprintln(w, "file content: %s", string(content))
+}
+
+func write(w http.ResponseWriter, r *http.Request) {
+  s := "Chris and I were here"
+  err := ioutil.WriteFile(FILEPATH, []byte(s), os.ModePerm)
+  if err != nil {
+    fmt.Fprintln(w, err)
+    return
+  }
+  fmt.Fprintln(w, "saved %s to %s successfully", s, FILEPATH)
+}
+
 func main()  {
+  http.HandleFunc("/read", read)
+  http.HandleFunc("/write", write)
   http.HandleFunc("/", handler)
+
   http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
